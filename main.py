961d07e9ff52fc65716d2e996a0bffcc69d526b9
XeenProof <72842381+XeenProof@users.noreply.github.com>
2024-10-03 11:09:40 -0400

    moving everything

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from util.JSONAccess import JSONAccess
-from DAO.BasicDAO import BasicDAO
+# from util.JSONAccess import JSONAccess
+import math
+from DAO.UserDAO import UserDAO
 
-dao = BasicDAO('database/user.json')
-dao.create(name="apple", date="no")
-print(dao.read())
-dao.update(name="apple pie", id=1)
-print(dao.read())
-dao.delete(id=1)
-print(dao.read())
+# dao = UserDAO()
+# dao.createUser(name="apple", email="no", password="no")
+# print(dao.getAllUsers())
+# dao.updateUser(id=0, name="apple pie")
+# print(dao.getAllUsers())
+# dao.update(name="apple pie", id=1)
+# print(dao.read())
+# dao.delete(id=0)
+# print(dao.read())
+# dao.delete(id=2)
+# print(dao.read())
+# dao.delete(id=3)
+# print(dao.read())
+li = [[1,2,3],[2,3,4],[3,4]]
+
+li2 = [x for y in li for x in y]
+print(li2)
+
 
 
 
